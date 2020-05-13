3d46b0ac48c9371f3b91541435b6b2a7cd2babc5
Stephan-MDD <stephan.mdd@gmail.com>
2020-05-13 13:14:23 +0200

    face recognition notebook

--- src/FaceDetector.py
+++ src/FaceDetector.py
@@ -1,8 +1,9 @@
 import cv2
+import math
 
 
 class FaceDetector:
-    """Some info"""
+    """some info"""
 
     classifier = None
 
@@ -18,16 +19,37 @@ class FaceDetector:
         classifier_path = self.classifier_paths[classifier_type]
         self.classifier = cv2.CascadeClassifier(classifier_path)
 
-    def crop(self, image):
+    def _detect(self, image):
         image_grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         detected_faces = self.classifier.detectMultiScale(image_grayscaled)
-        (x, y, w, h) = detected_faces[0]
+        if len(detected_faces) == 0:
+            return
+
+        tmp_diagonal = 0
+        tmp_face = None
+        for (x, y, w, h) in detected_faces:
+            diagonal = math.sqrt(w ** 2 + h ** 2)
+
+            if diagonal > tmp_diagonal:
+                tmp_diagonal = diagonal
+                tmp_face = (x, y, w, h)
+
+        return tmp_face
+
+    def crop(self, image):
+        face = self._detect(image)
+        if not face:
+            return
+
+        (x, y, w, h) = face
         cropped_image = image[y:y+h, x:x+w]
         return cropped_image
 
-    def detect(self, image):
-        image_grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        detected_faces = self.classifier.detectMultiScale(image_grayscaled)
-        (x, y, w, h) = detected_faces[0]
+    def mark(self, image):
+        face = self._detect(image)
+        if not face:
+            return
+
+        (x, y, w, h) = face
         cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
         return image
